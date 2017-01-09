e021d57ce0d49d0833a2896269906f96e1e6487f
Harsha Attray <harsh.attray@gmail.com>
2017-01-09 14:52:19 +0530

    feat: rotate and recursive branch function called

--- tree.js
+++ tree.js
@@ -1,3 +1,4 @@
+var slider;
 function setup(){
     createCanvas(400,400);
 }
@@ -7,4 +8,15 @@ function draw(){
     stroke(255);
     let length = 100;
     line(200,height,200,height-length);
+    translate(200,height);
+    branch(100);
+}
+//branch creation
+function branch(length){
+    line(0,0,0,-length);
+    translate(0,-length)
+    rotate(PI/4);
+    if(length>4){
+        branch(length * 0.67)
+    }
 }
