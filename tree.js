da44a44ddb3e20f4cc6d7b7807c17176efe62172
Harsha Attray <harsh.attray@gmail.com>
2017-01-09 15:04:45 +0530

    feat: slider based manipulation of fractal branch

--- tree.js
+++ tree.js
@@ -1,10 +1,13 @@
+var angle = PI/4;
 var slider;
 function setup(){
     createCanvas(400,400);
+    slider = createSlider(0,TWO_PI,PI/4,0.01);
 }
 function draw(){
     background(51);
     //baseline of the tree
+    angle = slider.value();
     stroke(255);
     let length = 100;
     line(200,height,200,height-length);
@@ -15,7 +18,7 @@ function draw(){
 function branch(length){
     line(0,0,0,-length);
     translate(0,-length)
-    rotate(PI/4);
+    rotate(angle);
     if(length>4){
         branch(length * 0.67)
     }
