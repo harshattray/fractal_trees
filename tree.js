b52aaf64e827237ee988df418874c77d7d3f75d4
Harsha Attray <harsh.attray@gmail.com>
2017-01-09 14:35:37 +0530

    feat: base of fractal tree defined

--- tree.js
+++ tree.js
@@ -1,1 +1,10 @@
-console.log("GFY");
+function setup(){
+    createCanvas(400,400);
+}
+function draw(){
+    background(51);
+    //baseline of the tree
+    stroke(255);
+    let length = 100;
+    line(200,height,200,height-length);
+}
